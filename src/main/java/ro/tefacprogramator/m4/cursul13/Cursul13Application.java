ba490b3004d6eecc1983722520fadcd747852a16
Ciprian Danila <ciprian.danila@yahoo.com>
2021-06-10 20:10:54 +0300

    Initial commit

---
+++ src/main/java/ro/tefacprogramator/m4/cursul13/Cursul13Application.java
@@ -0,0 +1,13 @@
+package ro.tefacprogramator.m4.cursul13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursul13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cursul13Application.class, args);
+	}
+
+}
---
+++ src/test/java/ro/tefacprogramator/m4/cursul13/Cursul13ApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.tefacprogramator.m4.cursul13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cursul13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
