a10213cc774f6f24242da05b18faf119be754b47
Ciprian Danila <ciprian.danila@yahoo.com>
2021-06-10 20:35:01 +0300

    Added new fields on the user enitity

--- src/main/java/ro/tefacprogramator/m4/cursul13/entity/User.java
+++ src/main/java/ro/tefacprogramator/m4/cursul13/entity/User.java
@@ -1,5 +1,20 @@
 package ro.tefacprogramator.m4.cursul13.entity;
 
 public class User {
+    private String firstName;
+    private String lastName;
     
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
 }
