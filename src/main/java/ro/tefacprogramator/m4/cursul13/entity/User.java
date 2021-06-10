a125a2cc1af8777ca749109c28cc715be608f56b
Ciprian Danila <ciprian.danila@yahoo.com>
2021-06-10 22:11:47 +0300

    New approach for the user entity

--- src/main/java/ro/tefacprogramator/m4/cursul13/entity/User.java
+++ src/main/java/ro/tefacprogramator/m4/cursul13/entity/User.java
@@ -1,20 +1,13 @@
 package ro.tefacprogramator.m4.cursul13.entity;
 
 public class User {
-    private String firstName;
-    private String lastName;
-    
-    public String getFirstName() {
-        return firstName;
-    }
-    public void setFirstName(String firstName) {
-        this.firstName = firstName + " ";
-    }
-    public String getLastName() {
-        return lastName;
-    }
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    private String fullName;
+
+    public String getFullName() {
+        return fullName;
     }
 
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
 }
